ca72803854ab36fd265710f942734b6af4213110
abidsutisna <87153031+abidsutisna@users.noreply.github.com>
2022-06-24 23:01:59 +0700

    entity penawaran

--- secondhand/src/main/java/com/secondhand/secondhand/models/entities/Penawaran.java
+++ secondhand/src/main/java/com/secondhand/secondhand/models/entities/Penawaran.java
@@ -1,5 +1,31 @@
 package com.secondhand.secondhand.models.entities;
 
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Penawaran {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long penawaranId;
+
+    @OneToOne(targetEntity = History.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "penawaranId", referencedColumnName = "penawaranId")
+    private Long notifikasiId;
+
+    private Long userId;
+
+    private Long produkId;
+
+    private Long hargaPenawaran;
+
 }
