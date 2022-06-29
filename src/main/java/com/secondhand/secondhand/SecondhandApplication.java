1cf8ae43d6d51f26e62bbdb60f915dd4c5c693cd
abidsutisna <87153031+abidsutisna@users.noreply.github.com>
2022-06-29 15:31:54 +0700

    cors

--- src/main/java/com/secondhand/secondhand/SecondhandApplication.java
+++ src/main/java/com/secondhand/secondhand/SecondhandApplication.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SecondhandApplication {
@@ -15,6 +18,17 @@ public class SecondhandApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	@Bean
+	@Deprecated
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/user/register/").allowedOrigins("http://localhost:3000"); 
+        	};
+    	};
 	}
 	
 }
