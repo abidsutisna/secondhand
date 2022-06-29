7118d4fa216c69150a5a2f260435a033f620d4eb
abidsutisna <87153031+abidsutisna@users.noreply.github.com>
2022-06-29 21:53:02 +0700

    security config

--- src/main/java/com/secondhand/secondhand/security/WebSecurityConfig.java
+++ src/main/java/com/secondhand/secondhand/security/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure (HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
-            .antMatchers("/user/register").permitAll()
+            .antMatchers("/user/register" , "/user/login").permitAll()
             .anyRequest().fullyAuthenticated()
             .and()
             .formLogin()
