6a31fe4092aac28124b3599c2cc17785aaef9e01
abidsutisna <87153031+abidsutisna@users.noreply.github.com>
2022-07-09 09:06:03 +0700

    turn off basic auth

--- src/main/java/com/secondhand/secondhand/utils/JwtUtil.java
+++ src/main/java/com/secondhand/secondhand/utils/JwtUtil.java
@@ -4,14 +4,26 @@ import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Service
+@Component
 public class JwtUtil {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtil.class);
+    
     private String secret = "secondhand";
     
     public String generateToken(String username) {
@@ -19,11 +31,34 @@ public class JwtUtil {
         return createToken(claims, username);
     }
 
+    public String getUserNameFromJwtToken (String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, secret).compact();
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try{
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e){
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e){
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 
 }
